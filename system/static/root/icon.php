21852a8ddfa28b5bddd7a25bfe9dfa44d5fe8f7e
hideki <hideki@ffa8ddc6-ed64-449d-89c4-29ad718b3e2b>
2009-12-05 08:29:59 +0000

    Removed review schema. Added specific icon for Apple iPhone/iPod touch.
    git-svn-id: http://dev.deskportal.net/source/live@12 ffa8ddc6-ed64-449d-89c4-29ad718b3e2b

--- system/static/root/icon.php
+++ system/static/root/icon.php
@@ -1,10 +1,21 @@
 <?php
-	if($_GET['type'] != 'png' && $_GET['type'] != 'ico') exit;
+	switch($_GET['type'])
+	{
+		case 'ico' : $icon = 'icon.ico'; break;
+
+		case 'png' : $icon = 'icon.png'; break;
+
+		case 'apple' : $icon = 'icon-apple.png' ;break;
+
+		default : exit; break;
+	}
+
 	require_once('../server/init.php'); #Load system library
 
 	#Set the requesting file manually
 	$_GET['_version'] = System_Static::app_conf('system', 'static', 'system_version');
-	$_GET['_self'] = 'system/'.str_replace('_', '/', System_Static::app_conf('system', 'static', 'system_version'))."/component/default/computer/graphic/icon.{$_GET['type']}";
+	$_GET['_self'] = 'system/'.str_replace('_', '/', System_Static::app_conf('system', 'static', 'system_version'))."/component/default/computer/graphic/$icon";
 
+	unset($icon);
 	System_Static::file_load('router-php.'.System_Static::app_conf('system', 'static', 'ext_php')); #Pass processing to the router script
 ?>
