a40d950d21d4c324de9674479efd625f3b87c319
hideki <hideki@ffa8ddc6-ed64-449d-89c4-29ad718b3e2b>
2010-02-26 13:13:11 +0000

    [about_1_0_0]  Fix for language matches on the select box.
    git-svn-id: http://dev.deskportal.net/source/live@66 ffa8ddc6-ed64-449d-89c4-29ad718b3e2b

--- about/1/0/0/client/run.js
+++ about/1/0/0/client/run.js
@@ -4,6 +4,9 @@
 		var preferred = $system.language.pref(); //Preferred language used in the interface
 		var list = $system.language.supported;
 
+		var picked; //See if the language matches fully on the options
+		var select = $system.node.id($id + '_option');
+
 		for(var i = 0; i < list.length; i++) //For each of the languages
 		{
 			var choice = document.createElement('option'); //Create an option tag
@@ -11,8 +14,19 @@
 			choice.value = list[i].code; //Find it's abbreviation
 			$system.node.text(choice, list[i].name); //Set it's formal name
 
-			if(preferred == choice.value) choice.selected = 'selected'; //If it is the chosen language, keep it selected
-			$system.node.id($id + '_option').appendChild(choice); //Add the selection to the interface
+			if(preferred == choice.value)
+			{
+				choice.selected = 'selected'; //If it is the chosen language, keep it selected
+				picked = true;
+			}
+
+			select.appendChild(choice); //Add the selection to the interface
+		}
+
+		if(!picked) //If no language matched, try to check on the major language part
+		{
+			preferred = preferred.replace(/[\-_].+$/, ''); //Strip language variance string
+			for(var i = 0; i < list.length; i++) if(preferred == list[i].code) select.value = preferred;
 		}
 
 		if($system.is.md5($global.user.ticket))
